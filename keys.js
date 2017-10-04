b64e7dece67e74e1a6faf5b38ef6a0884fd1a3c4
Loren <lorenrose@icloud.com>
2017-10-03 21:25:52 -0500

    initial set up
    files, functions, twitter api

---
+++ keys.js
@@ -0,0 +1,10 @@
+console.log("success");
+
+var twitterKeys = {
+	consumer_key: 'x8kZH64NjJ8BIMlBJ5PhAQQaB',
+	consumer_secret: 'zKgzsbC7yjeTYP3394bnJ9sclEaaLIZBHJSaoUiMGFT0AnQo7y',
+	access_token_key: '915388940512890880-QW96MhHxc0djgipqlmmOH2DzlyorkjV',
+	access_token_secret: 'pGDK6j7EdFhU80gR2mMYsAMYA6yIB4aaCqbIVH5k9LPHZ',
+}
+
+module.exports = twitterKeys;
---
+++ liri.js
@@ -0,0 +1,18 @@
+var keys = require("./twitterKeys");
+
+
+function retrieveTweets () {
+
+};
+
+function spotifySong () {
+
+};
+
+function movieGet () {
+
+};
+
+function command () {
+
+};
